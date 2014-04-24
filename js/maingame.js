d81bf8125a0fe1da183847fde2b047ff72bdaaeb
Elise Jakobsen <hei@elisejakob.no>
2014-04-24 16:04:05 +0200

    clouds <3

--- js/maingame.js
+++ js/maingame.js
@@ -6,7 +6,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'maingame');
 
 var menuState = {
     preload: function() {
-        game.load.image('background', 'http://procrastination.elisejakob.com/assets/background.png');
+        game.load.image('background', 'http://procrastination.elisejakob.com/assets/room-test-3.png');
         game.load.image('button', 'http://procrastination.elisejakob.com/assets/button-test.png');
     },
     create: function() {
@@ -30,12 +30,14 @@ var menuState = {
 var mainState = {
 
     preload: function() {
-        // Everything in this function will be executed at the beginning. That’s where we usually load the game’s assets (images, sounds, etc.)
-        game.load.image('background', 'http://procrastination.elisejakob.com/assets/room-test-1.png');
+        game.load.image('background', 'http://procrastination.elisejakob.com/assets/room-test-3.png');
         game.load.spritesheet('oblo', 'http://procrastination.elisejakob.com/assets/oblo-sprite-large.png', 76, 104, 12);
         game.load.image('speechbubble', 'http://procrastination.elisejakob.com/assets/speechbubble-2.png');
-        game.load.image('table', 'http://procrastination.elisejakob.com/assets/table.png');
-        game.load.image('iphone', 'http://procrastination.elisejakob.com/spill/flappy/assets/bird.png');
+        game.load.image('iphone', 'http://procrastination.elisejakob.com/assets/iphone.png');
+        game.load.image('trash', 'http://procrastination.elisejakob.com/assets/trash.png');
+        game.load.image('bed', 'http://procrastination.elisejakob.com/assets/bed.png');
+        game.load.image('desk', 'http://procrastination.elisejakob.com/assets/desk.png');
+        game.load.image('health', 'http://procrastination.elisejakob.com/assets/healthbar-test.png');
 
         // stop key events from propagating up to the browser
         var preventedKeys = [
@@ -49,18 +51,27 @@ var mainState = {
     },
 
     create: function() {
-        // This function will be called after the preload function. Here we set up the game, display sprites, add labels, etc.
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         // background sprite
         this.game.add.sprite(0, 0, 'background');
 
-        this.table = game.add.sprite(500, 450, 'table');
-        game.physics.arcade.enable(this.table);
-        this.table.anchor.setTo(0.5, 0.5);
-        this.table.body.immovable = true;
+        this.trash = game.add.sprite(50, 85, 'trash');
+        game.physics.arcade.enable(this.trash);
+        this.trash.anchor.setTo(0.5, 0.5);
+        this.trash.body.immovable = true;
 
-        this.iphone = game.add.sprite(100, 200, 'iphone');
+        this.bed = game.add.sprite(790, 340, 'bed');
+        game.physics.arcade.enable(this.bed);
+        this.bed.anchor.setTo(1, 0.5);
+        this.bed.body.immovable = true;
+
+        this.desk = game.add.sprite(200, 85, 'desk');
+        game.physics.arcade.enable(this.desk);
+        this.desk.anchor.setTo(0.5, 0.5);
+        this.desk.body.immovable = true;
+
+        this.iphone = game.add.sprite(100, 400, 'iphone');
         game.physics.arcade.enable(this.iphone);
         this.iphone.body.immovable = true;
 
@@ -79,36 +90,44 @@ var mainState = {
         this.oblo.animations.add('left', [6, 7, 8], 9, true);
         this.oblo.animations.add('right', [9, 10, 11], 9, true);
 
-        // speech bubble function
-        // setInterval(speechBubble, 3000);
+        this.health = game.add.sprite(10, 590, 'health');
+        this.health.anchor.setTo(0, 1);
     },
 
     update: function() {
-        // This is where we will spend the most of our time. This function is called 60 times per second to update the game.
+        // called 60 times per second to update the game
         var gameVar = this.game;
 
-        // spillliinekekr
-        this.game.physics.arcade.collide(this.oblo, this.table);
+        // game links
         this.game.physics.arcade.collide(this.oblo, this.iphone);
+        this.game.physics.arcade.collide(this.oblo, this.trash);
+        this.game.physics.arcade.collide(this.oblo, this.bed);
+        this.game.physics.arcade.collide(this.oblo, this.desk);
         if (gameVar.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
             if (game.physics.arcade.distanceBetween(this.oblo, this.iphone) < 100) {
                 this.game.state.start('flappy');
-            } else if (game.physics.arcade.distanceBetween(this.oblo, this.table) < 100) {
+            } else if (game.physics.arcade.distanceBetween(this.oblo, this.desk) < 100) {
                 this.game.state.start('avoid');
+            } else if ((game.physics.arcade.distanceBetween(this.oblo, this.trash) < 100) && ((this.trashLabel == null) || (this.trashLabel.exists == false))) {
+                // trash label
+                var style = { font: "16px unibody8", fill: "#222" };
+                this.trashLabel = this.game.add.text(game.world.centerX, 550, "Nope, there's only trash here!", style);
+                this.trashLabel.anchor.setTo(0.5, 0);
+                var labelDestroy = function(label){
+                    label.destroy();
+                };
+                setTimeout(labelDestroy, 1500, this.trashLabel);
+            } else if ((game.physics.arcade.distanceBetween(this.oblo, this.bed) < 200) && ((this.bedLabel == null) || (this.bedLabel.exists == false))) {
+                // bed label
+                var style = { font: "16px unibody8", fill: "#222" };
+                this.bedLabel = this.game.add.text(game.world.centerX, 550, "♡ Take a nap ♡", style);
+                this.bedLabel.anchor.setTo(0.5, 0);
+                var labelDestroy = function(label){
+                    label.destroy();
+                };
+                setTimeout(labelDestroy, 1500, this.bedLabel);
             };
         };
-
-        // table collision
-        /*this.game.physics.arcade.collide(this.oblo, this.table);
-        if (gameVar.input.keyboard.isDown(Phaser.Keyboard.ENTER) && this.table.body.wasTouching.up) {
-            this.game.state.start('avoid');
-        };*/
-
-        // iphone collision
-        /*this.game.physics.arcade.collide(this.oblo, this.iphone);
-        if (gameVar.input.keyboard.isDown(Phaser.Keyboard.ENTER) && this.iphone.body.touching) {
-            this.game.state.start('flappy');
-        };*/
         
         // make speech bubble
         //setInterval(function speechBubble () {
@@ -165,15 +184,6 @@ var mainState = {
             this.oblo.animations.stop();
         }
     },
-
-    /* speechBubble: function() {
-        if ((this.oblo.body.velocity.x == 0) && (this.oblo.body.velocity.y == 0) && (this.speechbubble == null)) {
-            this.speechbubble = game.add.sprite(100, 100, 'speechbubble');
-        } else if ((this.oblo.body.velocity.x > 0) || (this.oblo.body.velocity.y > 0)) {
-            this.speechbubble.kill();
-        }
-    }
-    */
 };
 
 
@@ -181,14 +191,12 @@ var mainState = {
 
 var avoidState = {
     preload: function() {
-        game.load.image('background', 'http://procrastination.elisejakob.com/assets/background.png');
+        this.game.stage.backgroundColor = '#9ae9ef';
         game.load.spritesheet('oblo', 'http://procrastination.elisejakob.com/assets/oblo-sprite-large.png', 76, 104, 12);
         game.load.image('workObject', 'http://procrastination.elisejakob.com/spill/flappy/assets/pipe.png');
     },
     create: function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        this.game.add.sprite(0,0, 'background');
 
         // oblo sprite
         this.oblo = game.add.sprite(game.world.centerX, 600, 'oblo');
@@ -200,6 +208,11 @@ var avoidState = {
         this.work = this.game.add.group();
         this.work.enableBody = true;
         this.timer = this.game.time.events.loop(1000, this.addWork, this);
+
+        // esc label info
+        var style = { font: "16px unibody8", fill: "#ffffff" };
+        this.escLabel = this.game.add.text(game.world.centerX, 20, "press esc to go back", style);
+        this.escLabel.anchor.setTo(0.5, 0);
     },
     update: function() {
         //  reset oblo movement
@@ -252,7 +265,7 @@ var avoidState = {
 var flappyState = {
     preload: function() {
         // bg color
-        this.game.stage.backgroundColor = '#71c5cf';
+        this.game.stage.backgroundColor = '#9ae9ef';
         // load bird sprite
         this.game.load.image('bird', 'http://procrastination.elisejakob.com/spill/flappy/assets/bird.png');
         // load pipe sprite
@@ -287,7 +300,7 @@ var flappyState = {
         this.labelScore = this.game.add.text(20, 20, "0", style);
 
         // esc label info
-        var style = { font: "8px unibody8", fill: "#ffffff" };
+        var style = { font: "16px unibody8", fill: "#ffffff" };
         this.escLabel = this.game.add.text(game.world.centerX, 20, "press esc to go back", style);
         this.escLabel.anchor.setTo(0.5, 0);
     },
